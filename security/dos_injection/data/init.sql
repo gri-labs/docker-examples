331ece1070d625869ce72b4b60067096a39b4e35
Ricardo Olivera <ricardoolivera.etpc@gmail.com>
2023-05-30 16:16:01 +0200

    Add wordpreess

--- security/dos_injection/data/init.sql
+++ security/dos_injection/data/init.sql
@@ -9,3 +9,7 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (username) VALUES ('user1');
+INSERT INTO users (username) VALUES ('user2');
+INSERT INTO users (username) VALUES ('user3');
+INSERT INTO users (username) VALUES ('user4');
+INSERT INTO users (username) VALUES ('user5');
