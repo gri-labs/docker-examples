fb53ef8621e98af840f3623ca0c0e61e08e725cb
Ricardo Olivera <ricardoolivera.etpc@gmail.com>
2023-06-09 18:19:27 +0200

    new function user group example

---
+++ groups/group_example/app.py
@@ -0,0 +1,32 @@
+# Importa la clase flask del modulo flask
+from flask import Flask
+# Importa el modulo logging
+import logging
+
+# Se crea una instancia de la clase flask llamada app y esta variable app se utiliza para configurar el resto del programa
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+# Define una función llamada hell_world
+# Nos ayuda a encapsular el código, mantener funcionalidades...
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/usuario', methods=['GET'])
+# Define una función llamada hell_world
+# Nos ayuda a encapsular el código, mantener funcionalidades...
+def hello_world():
+    return 'Hello World!'
+
+
+## Crea una nueva función para el usuario 2
+
+
+# Arranque del servidor o inicio del programa
+if __name__ == '__main__':
+    # Se configura el log
+    logging.basicConfig(filename='request.log', level=logging.DEBUG)
+    # Se ejecuta el servidor
+    app.run(host='0.0.0.0', port=8080)
